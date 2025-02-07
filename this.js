09267f0924d9147d277385eb50cee01711108ae6
Parth Lahoti <parthlahoti5404@gmail.com>
2025-02-07 17:20:23 +0530

    अभ्यास 2

--- this.js
+++ this.js
@@ -1,10 +1,42 @@
 
    "use strict"
-console.log(this);
+// console.log(this);
 
 
 function x(){
  
     console.log(this)
 }
-x()
+// x()
+// window.x()
+
+const obj={
+    a:10,
+    // x:function(){
+    //     console.log(this.a)
+    // }
+    // x:()=>{
+    //     console.log(this)
+    // }
+    x:function(){
+        const y=()=>{
+            console.log(this.a)
+        }
+        y();
+    }
+}
+obj.x()
+// console.log(typeof(obj.x()))
+
+// const student={
+//     name:"sher",
+//     print:function(){
+//         console.log(this)
+//     }
+// }
+
+// const student2={
+//     name:"abcd"
+// }
+
+// student.print.call(student2)
